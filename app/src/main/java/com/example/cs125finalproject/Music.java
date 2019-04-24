bca1d9bf834658f0766751739adadae32dc8c3c5
Clayton Douglas <ckd6@illinois.edu>
2019-04-23 20:12:19 -0500

    Initial commit

---
+++ app/src/main/java/com/example/cs125finalproject/Music.java
@@ -0,0 +1,11 @@
+package com.example.cs125finalproject;
+
+/**
+ * Generates a new chord progression based on user inputs.
+ */
+public class Music {
+    /**
+     * Scale to construct the chord progression.
+     */
+    private String scale;
+}
